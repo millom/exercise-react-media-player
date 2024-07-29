787eb7b2736268089b314f76b4c7701e80fc382d
Mikael Fiala-Lundstrom <millom@gmail.com>
2024-07-29 13:16:10 +0200

    Fix header design

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,7 +1,14 @@
 import { ReactElement } from "react";
 
 import "../css/css-grid.css";
+import "../css/Header.css";
 
 export function Header(): ReactElement {
-  return <h1 className="header">Header</h1>;
+  return (
+    <div className="header">
+      <span className="material-symbols-outlined">arrow_back_ios</span>
+      <h1>Playlist - Moody</h1>
+      <span className="material-symbols-outlined">more_vert</span>
+    </div>
+  );
 }
