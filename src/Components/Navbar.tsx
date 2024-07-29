e82c4563800a4ed3b5b62ebed02aac80aafe45e1
Mikael Fiala-Lundstrom <millom@gmail.com>
2024-07-29 19:59:29 +0200

    Add interfaces

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,7 +1,18 @@
 import { ReactElement } from "react";
+// import { IBook } from "../interfaces";
+import { NavItem } from "./NavItem";
+import { books } from "../data";
 
 import "../css/css-grid.css";
 
+// const data2: IBook[] = data;
+
 export function Navbar(): ReactElement {
-  return <div className="navbar">Navbar</div>;
+  return (
+    <nav className="navbar">
+      {books.map((book) => (
+        <NavItem key={book.id} book={book} />
+      ))}
+    </nav>
+  );
 }
---
+++ src/interfaces.ts
@@ -0,0 +1,11 @@
+// export interface IMusicAlbums {
+//   data: IMusicAlbum[];
+// }
+
+export interface IBook {
+  id: number;
+  title: string;
+  author: string;
+  series: string;
+  src: string;
+}
