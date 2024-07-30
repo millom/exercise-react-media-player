8cecd754daa260c1d5ad5c974915896ecaa7fd59
Mikael Fiala-Lundstrom <millom@gmail.com>
2024-07-30 14:45:31 +0200

    Fix layout for Content

--- src/Components/Content.tsx
+++ src/Components/Content.tsx
@@ -17,11 +17,15 @@ export function Content({ books, index }: IContentProps): ReactElement {
 
   return (
     <div className="content">
-      <figure className="figure">
-        <img className="image" src={books[index].src} alt={books[index].src} />
+      <figure className="big-figure">
+        <img
+          className="big-image"
+          src={books[index].src}
+          alt={books[index].src}
+        />
       </figure>
       <article className="article">
-        <h2 className="header">Title: {books[index].title}</h2>
+        <h2 className="paragraph">Title: {books[index].title}</h2>
         <h2 className="paragraph">Author: {books[index].author}</h2>
         <h2 className="paragraph">Series: {books[index].series} series</h2>
       </article>
